a0b97a1e270f3e2b79abedddb04dee0aee222f59
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-15 22:39:54 +0530

    Home page module

---
+++ FrontEnd/job-app-tracker/src/core/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './CSS/style.css'
+
+function home() {
+    return (
+        <div>
+            <div className="heading">
+                <h1>It's A Application Tracker</h1>
+                <h5>Where you can track any kind of application</h5>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default home
