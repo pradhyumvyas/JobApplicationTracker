4de34124558760393d8f856ee36e8ab87c8bbe46
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-17 16:15:11 +0530

    Signup for done

--- FrontEnd/job-app-tracker/src/user/Signup.js
+++ FrontEnd/job-app-tracker/src/user/Signup.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Button} from '@material-ui/core'
 
 function Signup() {
     return (
         <div>
-            <h1>Signup</h1>
+            <div className="signup">
+                <div className="signupForm">
+                    <h2>Sign Up</h2>
+                    <form action="">
+                        <label htmlFor="">First Name</label>
+                        <input type="text" placeholder="Enter First Name" />
+
+                        <label htmlFor="">Last Name</label>
+                        <input type="text" placeholder="Enter Last Name" />
+
+                        <label htmlFor="">Email</label>
+                        <input type="text" placeholder="Enter your email" />
+
+                        <label htmlFor="">Username</label>
+                        <input type="text" placeholder="username" />
+
+                        <label htmlFor="">Password</label>
+                        <input type="text" placeholder="Enter Password" />
+
+                        <Button variant="contained" color="primary" >Sign up</Button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
