be59cb4104ec08a8f3365830822f4b772a1b29e9
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-18 20:16:59 +0530

    Added dashboard

--- FrontEnd/job-app-tracker/src/Routes.js
+++ FrontEnd/job-app-tracker/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Home from './core/home'
+import Dashboard from './user/Dashboard';
 import Signin from './user/Signin'
 import Signup from './user/Signup';
 
@@ -13,6 +14,7 @@ function Routes() {
             <Route path="/" exact component={Home} ></Route>
             <Route path="/signin" exact component={Signin} ></Route>
             <Route path="/signup" exact component={Signup} ></Route>
+            <Route path="/dashboard" exact component={Dashboard} ></Route>
         </Switch>
         </BrowserRouter>
     )
