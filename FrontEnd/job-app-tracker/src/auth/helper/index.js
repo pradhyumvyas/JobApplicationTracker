da9ef5953c1f1cdc0f156756da7dc75fa14f37b1
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-23 21:45:59 +0530

    set values and state for signup

---
+++ FrontEnd/job-app-tracker/src/auth/helper/index.js
@@ -0,0 +1,18 @@
+import {API} from "../../Backend"
+
+
+
+export const signup = user => {
+    return fetch(`${API}user/`, {
+        method:"POST",
+        headers:{
+            Accept:"application/json",
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(user)
+    })
+    .then((response) =>{
+        return response.json();
+    })
+    .catch(err => console.log(err))
+}
--- FrontEnd/job-app-tracker/src/user/Signup.js
+++ FrontEnd/job-app-tracker/src/user/Signup.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button} from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
+import {signup} from '../auth/helper'
 
 
 function Signup() {
 
     const history = useHistory();
     const loginPush = () => history.push('/signin')
+    
+    const [values, setValues] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        username:"",
+        password:"",
+        error:"",
+        sucess:false,
+    })
+
+    const {firstName, lastName, email, username, password, error, sucess} = values;
 
     return (
         <div>
@@ -15,19 +28,19 @@ function Signup() {
                     <h2>Sign Up</h2>
                     <form action="">
                         <label htmlFor="">First Name</label>
-                        <input type="text" placeholder="Enter First Name" />
+                        <input type="text" placeholder="Enter First Name" value={firstName} />
 
                         <label htmlFor="">Last Name</label>
-                        <input type="text" placeholder="Enter Last Name" />
+                        <input type="text" placeholder="Enter Last Name" value={lastName} />
 
                         <label htmlFor="">Email</label>
-                        <input type="text" placeholder="Enter your email" />
+                        <input type="text" placeholder="Enter your email" value={email} />
 
                         <label htmlFor="">Username</label>
-                        <input type="text" placeholder="username" />
+                        <input type="text" placeholder="username" value={username} />
 
                         <label htmlFor="">Password</label>
-                        <input type="text" placeholder="Enter Password" />
+                        <input type="text" placeholder="Enter Password" value={password} />
 
                         <Button variant="contained" color="primary" >Sign up</Button>
                         <Button color="primary" onClick={loginPush} >Login now ?</Button>
