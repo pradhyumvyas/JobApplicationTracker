dd7b8e62636f06c44c8d5a98c6c36cf5330b946e
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-26 21:57:24 +0530

    Signup done from frontend

--- FrontEnd/job-app-tracker/src/user/Signup.js
+++ FrontEnd/job-app-tracker/src/user/Signup.js
@@ -55,44 +55,53 @@ function Signup() {
             .catch((e) => console.log(e))
     }
 
-    return (
-        <div>
-            <div className="signup">
-                <div className="signupForm">
-                    <h2>Sign Up</h2>
-                    <form action="">
-                        <label htmlFor="" className="text-light">First Name</label>
-                        <input required type="text" placeholder="Enter First Name" 
-                        onChange={handleChange("firstName")}
-                        value={firstName} required />
-
-                        <label htmlFor="">Last Name</label>
-                        <input type="text" placeholder="Enter Last Name"
-                        onChange={handleChange("lastName")} 
-                        value={lastName} />
-
-                        <label htmlFor="">Email</label>
-                        <input type="text" placeholder="Enter your email" 
-                        onChange={handleChange("email")} required
-                        value={email} />
-
-                        <label htmlFor="">Username</label>
-                        <input type="text" placeholder="username" 
-                        onChange={handleChange("username")} required
-                        value={username} />
-
-                        <label htmlFor="">Password</label>
-                        <input type="password" placeholder="Enter Password" 
-                        onChange={handleChange("password")} required
-                        value={password} />
-
-                        <Button variant="contained" color="primary" onClick={onSubmit}>Sign up</Button>
-                        <Button color="primary" onClick={loginPush} >Login now ?</Button>
-
-                    </form>
+    const signupForm = () => {
+        return (
+            <div>
+                <div className="signup">
+                    <div className="signupForm">
+                        <h2>Sign Up</h2>
+                        <form action="">
+                            <label htmlFor="" className="text-light">First Name</label>
+                            <input required type="text" placeholder="Enter First Name" 
+                            onChange={handleChange("firstName")}
+                            value={firstName} required />
+    
+                            <label htmlFor="">Last Name</label>
+                            <input type="text" placeholder="Enter Last Name"
+                            onChange={handleChange("lastName")} 
+                            value={lastName} />
+    
+                            <label htmlFor="">Email</label>
+                            <input type="text" placeholder="Enter your email" 
+                            onChange={handleChange("email")} required
+                            value={email} />
+    
+                            <label htmlFor="">Username</label>
+                            <input type="text" placeholder="username" 
+                            onChange={handleChange("username")} required
+                            value={username} />
+    
+                            <label htmlFor="">Password</label>
+                            <input type="password" placeholder="Enter Password" 
+                            onChange={handleChange("password")} required
+                            value={password} />
+    
+                            <Button variant="contained" color="primary" onClick={onSubmit}>Sign up</Button>
+                            <Button color="primary" onClick={loginPush} >Login now ?</Button>
+    
+                        </form>
+                    </div>
                 </div>
             </div>
+        )
+    }
+
+    return (
+        <div className="">
+            {signupForm()}
         </div>
+            
     )
 }
 
