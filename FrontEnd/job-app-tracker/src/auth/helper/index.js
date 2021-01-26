a95b9f69c0b0c64ce7179acb39b46d9d299d9cc0
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-01-27 00:10:31 +0530

    signin from index

--- FrontEnd/job-app-tracker/src/auth/helper/index.js
+++ FrontEnd/job-app-tracker/src/auth/helper/index.js
@@ -1,5 +1,8 @@
 import {API} from "../../Backend"
 
+const mailOnSignup = () =>{
+    return 
+}
 
 
 export const signup = user => {
@@ -19,6 +22,15 @@ export const signup = user => {
 
 export const signin = user =>{
     return fetch(`${API}/login`, {
-        
+        method:"POST",
+        headers:{
+            Accept:"application/json",
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(user)
     })
+    .then((response) =>{
+        return response.json();
+    })
+    .catch(err => console.log(err))
 }
