1104fe9a2acc3f9d1c821de7a99a5bf1967cd36e
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-05 00:05:23 +0530

    authentication methods

--- FrontEnd/job-app-tracker/src/auth/helper/index.js
+++ FrontEnd/job-app-tracker/src/auth/helper/index.js
@@ -47,3 +47,42 @@ export const signin = user =>{
         return response.json()
     })
 }
+
+export const authenticate = (data, next)  =>{
+    if(typeof window !== undefined){
+        localStorage.setItem("jwt", JSON.stringify(data))
+        next();
+    }
+}
+
+export const isAuthenticated = () =>{
+    if(typeof window === undefined){
+        return false
+    }
+
+    if(localStorage.getItem("jwt")){
+        return JSON.parse(localStorage.getItem("jwt"))
+        
+        //TODO: Compare JWT with database json token
+        
+    } else{
+        return false; 
+    }
+}
+
+// export const signout = next => {
+//     const userId = isAuthenticated() && isAuthenticated().user.id
+
+//     if(typeof window !== undefined){
+//         localStorage.removeItem("jwt")
+
+//         return fetch(`${API}user/logout/${userId}/`, {
+//             method:"GET"
+//         })
+//         .then(response => {
+//             console.log("Signout Success")
+//             next();
+//         })
+//         .catch(err => console.log(err))
+//     }
+// }
