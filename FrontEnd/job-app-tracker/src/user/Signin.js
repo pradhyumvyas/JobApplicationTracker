bb55a05c74984cb6d4824a8d80544e4542523e89
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-04 19:08:36 +0530

    done with login error and signup message

--- FrontEnd/job-app-tracker/src/user/Signin.js
+++ FrontEnd/job-app-tracker/src/user/Signin.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { signin } from '../auth/helper';
+import { Alert, AlertTitle } from '@material-ui/lab';
+
 
 function Signin() {
 
     const history = useHistory();
     const signupPush = () => history.push('/signup')
 
-    return (
-        <div>
+    const [values, setValues] = useState({
+        error:"",
+        sucess:false,
+    })
+    const {error, sucess} = values;
+
+    const errorMessage = () =>{
+        if (values.error === true){
+            return(
+                <Alert severity="error" onClose={() => {}}>
+                    <AlertTitle>Error</AlertTitle>
+                    Something Went Wrong please try again with different — <strong>Email and Username!</strong>
+                </Alert>
+            )
+        }
+    }
+
+
+    const successMessage = () =>{
+        if(values.sucess === true){
+            return (
+                <Alert severity="success" onClose={() => {}}>
+                    <AlertTitle>Success</AlertTitle>
+                    <strong> <a >Login Successfull</a> </strong>
+                </Alert>
+            )
+        }
+    }
+
+    const onLoginSubmit = () => {
+        return(
+            setValues({
+                ...values,
+                sucess:true
+            // alert("gell")
+        })
+        )
+    }
+
+    const signinForm = () =>{
+        return(
             <div className="signinForm">
                 <div className="signin">
                     <form action="">
@@ -20,14 +61,22 @@ function Signin() {
                         <label htmlFor="">password</label>
                         <input type="password" placeholder="Enter Password"/>
 
-                        <Button variant="contained" color="primary" >Login</Button>
+                        <Button variant="contained" color="primary" onClick={onLoginSubmit} >Login</Button>
 
                         <Button variant="contained" color="primary" onClick={signupPush} >Sign Up</Button>
                         <Button color="primary" >Forgot Password ?</Button>
-                        
+
                     </form>
                 </div>
             </div>
+        )
+    }
+
+    return(
+        <div>
+            {successMessage()}            
+            {errorMessage()}
+            {signinForm()}
         </div>
     )
 }
