722b5c9ef994ae5b31ff5a343dc3ee20f3b58fa6
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-08 18:15:15 +0530

    Working on Logout

--- FrontEnd/job-app-tracker/src/user/Dashboard.js
+++ FrontEnd/job-app-tracker/src/user/Dashboard.js
@@ -7,17 +7,19 @@ function Dashboard() {
     const myNavbar = () =>{
         return(
             <div className="myNav">
-                <AppBar position="static">
-                    <Toolbar>
-                        {/* <IconButton edge="start" className="" color="inherit" aria-label="menu"> */}
-                        {/* <MenuIcon /> */}
-                        {/* </IconButton> */}
-                        {/* <Typography variant="h6" className="">
-                            News
-                        </Typography> */}
-                        <Button color="inherit" className="signout">Signout</Button>
-                    </Toolbar>
-                </AppBar>
+                <div>
+                    <AppBar position="static">
+                        <Toolbar>
+                            {/* <IconButton edge="start" className="" color="inherit" aria-label="menu"> */}
+                            {/* <MenuIcon /> */}
+                            {/* </IconButton> */}
+                            {/* <Typography variant="h6" className="">
+                                News
+                            </Typography> */}
+                            <Button  color="secondary" variant="contained" className="signout pull-right">Signout</Button>
+                        </Toolbar>
+                    </AppBar>
+                </div>
             </div>
         )
     }
