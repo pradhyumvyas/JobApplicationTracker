6172deb1458ea17016b59cee94d231aa9af9dcae
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-09 13:23:05 +0530

    Signout done

--- FrontEnd/job-app-tracker/src/user/Dashboard.js
+++ FrontEnd/job-app-tracker/src/user/Dashboard.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core'
 // import MenuIcon from '@material-ui/icons/Menu';
+import {signout, isAuthenticated} from '../auth/helper/index'
+import { useHistory } from 'react-router-dom'
 
 function Dashboard() {
+    const history = useHistory();
+    const signoutPush = () => history.push('/')
+
+    const homeRedirect = () =>{
+        if(isAuthenticated()){
+            signout()
+            signoutPush()
+        }
+        else{
+            alert("No user is login")
+        }
+        return(console.log("here"))
+    }
 
     const myNavbar = () =>{
         return(
@@ -16,7 +31,7 @@ function Dashboard() {
                             {/* <Typography variant="h6" className="">
                                 News
                             </Typography> */}
-                            <Button  color="secondary" variant="contained" className="signout pull-right">Signout</Button>
+                            <Button  color="secondary" variant="contained" onClick={homeRedirect} className="signout pull-right">Signout</Button>
                         </Toolbar>
                     </AppBar>
                 </div>
