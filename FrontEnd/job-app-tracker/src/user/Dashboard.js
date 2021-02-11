4da4a250c50da927fb8097760415ef8f7e8733f4
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-11 20:11:55 +0530

    Apply data form

--- FrontEnd/job-app-tracker/src/user/Dashboard.js
+++ FrontEnd/job-app-tracker/src/user/Dashboard.js
@@ -25,15 +25,58 @@ function Dashboard() {
                 <div>
                     <AppBar position="static">
                         <Toolbar>
-                            {/* <IconButton edge="start" className="" color="inherit" aria-label="menu"> */}
-                            {/* <MenuIcon /> */}
-                            {/* </IconButton> */}
-                            {/* <Typography variant="h6" className="">
-                                News
-                            </Typography> */}
                             <Button  color="secondary" variant="contained" onClick={homeRedirect} className="signout pull-right">Signout</Button>
                         </Toolbar>
                     </AppBar>
+                </div>
+            </div>
+        )
+    }
+
+    const applyForm = () =>{
+        return(
+            <div className="applyForm">
+                <div className="">
+                <form action="">
+                            <label htmlFor="" className="text-light">Company Name</label>
+                            <input required type="text" placeholder="Company Name" 
+                            onChange={handleChange("companyName")}
+                            value={companyName} required />
+    
+                            <label htmlFor="">Apply Date</label>
+                            <input type="text" placeholder="Date of Apply"
+                            onChange={handleChange("applyDate")} 
+                            value={applyDate} />
+    
+                            <label htmlFor="">Location</label>
+                            <input type="text" placeholder="Where is Location" 
+                            onChange={handleChange("location")} required
+                            value={location} />
+    
+                            <label htmlFor="">Acknowledge</label>
+                            <input type="text" placeholder="Expected date of response" 
+                            onChange={handleChange("responseDate")} required
+                            value={responseDate} />
+
+                            <label htmlFor="">Profile</label>
+                            <input type="text" placeholder="Which profile you have applied" 
+                            onChange={handleChange("profile")} required
+                            value={profile} />
+
+                            <label htmlFor="">Job or Intern</label>
+                            <input type="text" placeholder="J or I" 
+                            onChange={handleChange("jobType")} required
+                            value={jobType} />
+
+                            <label htmlFor="">Status</label>
+                            <input type="text" placeholder="R S and P" 
+                            onChange={handleChange("status")} required
+                            value={status} />
+    
+                            <Button variant="contained" color="primary" onClick={onSubmit}>Sign up</Button>
+                            <Button color="primary" onClick={loginPush} >Login now ?</Button>
+    
+                        </form>
                 </div>
             </div>
         )
