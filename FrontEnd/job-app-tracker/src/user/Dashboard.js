c3803fa73ca3f1ad8b4bf2661db99fa35f9c4f22
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-02-19 18:26:41 +0530

    application data modal done

--- FrontEnd/job-app-tracker/src/user/Dashboard.js
+++ FrontEnd/job-app-tracker/src/user/Dashboard.js
@@ -3,25 +3,11 @@ import {AppBar, Toolbar, Typography, Button, IconButton, Modal} from '@material-
 // import MenuIcon from '@material-ui/icons/Menu';
 import {signout, isAuthenticated} from '../auth/helper/index'
 import { useHistory } from 'react-router-dom'
+import modalForm from './helper/modalForm'
 
-function Dashboard() {
+function Dashboard(){
     const history = useHistory();
     const signoutPush = () => history.push('/')
-
-    const [values, setValues] = useState({
-        companyName:"",
-        applyDate:"",
-        responseDate:"",
-        profile:"",
-        jobType:"",
-        status:"",
-        location:""
-    })
-    const {companyName, applyDate, responseDate, profile, jobType, status, location} = values;
-
-    const handleChange = name => event =>{
-        setValues({...values, error:false, [name]:event.target.value})
-    }
 
 
     const homeRedirect = () =>{
@@ -49,61 +35,13 @@ function Dashboard() {
         )
     }
 
-    const onSubmit = () =>{
-        return alert("hee")
-    }
-
-    const applyForm = () =>{
-        return(
-            <div className="applyForm">
-                <div className="dataForm">
-                <form action="">
-                            <label htmlFor="" className="text-light">Company Name</label>
-                            <input required type="text" placeholder="Company Name" 
-                            onChange={handleChange("companyName")}
-                            value={companyName} required />
+   
     
-                            <label htmlFor="">Apply Date</label>
-                            <input type="text" placeholder="Date of Apply"
-                            onChange={handleChange("applyDate")} 
-                            value={applyDate} />
-    
-                            <label htmlFor="">Location</label>
-                            <input type="text" placeholder="Where is Location" 
-                            onChange={handleChange("location")} required
-                            value={location} />
-    
-                            <label htmlFor="">Acknowledge</label>
-                            <input type="text" placeholder="Expected date of response" 
-                            onChange={handleChange("responseDate")} required
-                            value={responseDate} />
-
-                            <label htmlFor="">Profile</label>
-                            <input type="text" placeholder="Which profile you have applied" 
-                            onChange={handleChange("profile")} required
-                            value={profile} />
-
-                            <label htmlFor="">Job or Intern</label>
-                            <input type="text" placeholder="J or I" 
-                            onChange={handleChange("jobType")} required
-                            value={jobType} />
-
-                            <label htmlFor="">Status</label>
-                            <input type="text" placeholder="R S and P" 
-                            onChange={handleChange("status")} required
-                            value={status} />
-    
-                            <Button variant="contained" color="primary" onClick={onSubmit}>Done</Button>
-                        </form>
-                </div>
-            </div>
-        )
-    }
     return (
         <div>
             {myNavbar()}
-            {/* <Button variant="contained" color="Primary" onClick={applyForm}>Add Data</Button> */}
-            {applyForm()}
+            {modalForm()}
+            {/* <modalForm /> */}
         </div>
     )
 }
---
+++ FrontEnd/job-app-tracker/src/user/helper/modalForm.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react'
+import {Button,Modal, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from '@material-ui/core'
+
+function ModalForm (){
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+    setOpen(true);
+    };
+
+    const handleClose = () => {
+    setOpen(false);
+    };
+
+    const [values, setValues] = useState({
+        companyName:"",
+        applyDate:"",
+        responseDate:"",
+        profile:"",
+        jobType:"",
+        status:"",
+        location:""
+    })
+    const {companyName, applyDate, responseDate, profile, jobType, status, location} = values;
+
+    const handleChange = name => event =>{
+        setValues({...values, error:false, [name]:event.target.value})
+    }
+
+    const onSubmit = () =>{
+        return alert("hee")
+    }
+
+
+    return (
+        <div>
+        <Button variant="contained" color="primary" onClick={handleClickOpen}>
+            Open form dialog
+        </Button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Add Application Data</DialogTitle>
+            <DialogContent>
+            <div className="applyForm">
+                <div className="dataForm">
+                <form action="">
+                            <label htmlFor="" className="text-light">Company Name</label>
+                            <input required type="text" placeholder="Company Name" 
+                            onChange={handleChange("companyName")}
+                            value={companyName} required />
+    
+                            <label htmlFor="">Apply Date</label>
+                            <input type="text" placeholder="Date of Apply"
+                            onChange={handleChange("applyDate")} 
+                            value={applyDate} />
+    
+                            <label htmlFor="">Location</label>
+                            <input type="text" placeholder="Where is Location" 
+                            onChange={handleChange("location")} required
+                            value={location} />
+    
+                            <label htmlFor="">Acknowledge</label>
+                            <input type="text" placeholder="Expected date of response" 
+                            onChange={handleChange("responseDate")} required
+                            value={responseDate} />
+
+                            <label htmlFor="">Profile</label>
+                            <input type="text" placeholder="Which profile you have applied" 
+                            onChange={handleChange("profile")} required
+                            value={profile} />
+
+                            <label htmlFor="">Job or Intern</label>
+                            <input type="text" placeholder="J or I" 
+                            onChange={handleChange("jobType")} required
+                            value={jobType} />
+
+                            <label htmlFor="">Status</label>
+                            <input type="text" placeholder="R S and P" 
+                            onChange={handleChange("status")} required
+                            value={status} />
+                        </form>
+                </div>
+            </div>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+            <Button variant="contained" color="primary" onClick={onSubmit}>Done</Button>
+            </DialogActions>
+        </Dialog>
+        </div>
+    );
+}
+
+export default ModalForm
