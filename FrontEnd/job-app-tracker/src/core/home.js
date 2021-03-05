10f0b69074347aff939cb5766129b5eba0e6ee14
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-03-05 22:27:50 +0530

    Home Page UI Designing

--- FrontEnd/job-app-tracker/src/core/home.js
+++ FrontEnd/job-app-tracker/src/core/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './CSS/style.css'
 import {Button} from '@material-ui/core'
 import { useHistory } from "react-router-dom";
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 
 
@@ -13,14 +14,26 @@ function Home() {
     return (
         <div>
             <div className="heading">
-                <h1>It's A Application Tracker</h1>
+                <h1>It's  A  Application  Tracker</h1>
                 <h5>Where you can track any kind of application</h5>
+            </div>
+
+            <div className="subHeading">
+                <h2>Want To Try This Platform</h2>
+                <h3>If Yesss..!</h3>
+                <h4>Go To The Below Buttons !</h4>
+                {/* {ArrowDownwardIcon} */}
             </div>
 
             <div className="userAuth">
                 <Button variant="contained" color="primary" onClick={signupRoute} >Sign Up</Button>
                 <Button variant="contained" color="primary"  onClick={signinRoute} >Login</Button>
 
+            </div>
+
+            <div className="footer">
+                <h3>Made with ❤</h3>
+                <a href="https://pradhyumvyas.github.io" target="_blank">I'm here</a>
             </div>
 
 
