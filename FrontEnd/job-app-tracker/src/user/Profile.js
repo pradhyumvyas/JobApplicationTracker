b5ae78addbd1669433aee59abd0462d66adf9e03
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-05-20 19:53:52 +0530

    Profile Section

--- FrontEnd/job-app-tracker/src/user/Profile.js
+++ FrontEnd/job-app-tracker/src/user/Profile.js
@@ -4,7 +4,17 @@ function Profile() {
     return (
         <div>
             <MyNavbar />
-            <h1>Profile</h1>
+            {/* <h1>Profile</h1> */}
+            <div className="User Profile">
+                <div className="card">
+                    <div className="leftProfile">
+                        <h1>IMG</h1>
+                    </div>
+                    <div className="rightProfile">
+                        <h1>User Details</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
