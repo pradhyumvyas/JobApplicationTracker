2b295245ecd833a06a3e457a5eb890f921b18659
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2021-05-22 00:07:25 +0530

    Profile Section

--- FrontEnd/job-app-tracker/src/user/Profile.js
+++ FrontEnd/job-app-tracker/src/user/Profile.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { MyNavbar } from '../mainData/Dashboard'
+import download from "../images/download.png"
 function Profile() {
+
+    const currentUserData = JSON.parse(localStorage.getItem('jwt'))
+    // console.log(currentUserData['user']);
+
     return (
         <div>
             <MyNavbar />
-            {/* <h1>Profile</h1> */}
-            <div className="User Profile">
+            <div className="userProfile">
                 <div className="card">
                     <div className="leftProfile">
-                        <img src="" alt=""/>
+                        <img src={download} alt=""/>
                     </div>
                     <div className="rightProfile">
                         <h1>User Details</h1>
+                        <h3>Name: {currentUserData['user'].firstName} {currentUserData['user'].lastName}</h3>
+                        <h3>Email: {currentUserData['user'].email}</h3>
+                        <h3>Username: {currentUserData['user'].username}</h3>
                     </div>
                 </div>
             </div>
